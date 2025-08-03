f2f29d553f2708d457aa255b0f4e6ca43029c017
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2025-08-03 20:03:18 +0530

    feat: ios

--- src/utils/generateThumbnail.ts
+++ src/utils/generateThumbnail.ts
@@ -1,8 +1,23 @@
+import { Platform } from 'react-native';
 import { createThumbnail } from 'react-native-create-thumbnail';
 
 export const generateThumbnail = async (videoPath: string) => {
+
+  if(!videoPath){
+    return;
+  }
+
+  const uri = Platform.select({
+    ios: videoPath.replace(/^file:\/\/file:\/\//, ''),
+    android: videoPath,
+  });
+
+  if (!uri) {
+    return;
+  }
+  
   try {
-    const { path } = await createThumbnail({ url: videoPath });
+    const { path } = await createThumbnail({ url: uri });
     return path;
   } catch (err) {
     console.error('Thumbnail error:', err);
